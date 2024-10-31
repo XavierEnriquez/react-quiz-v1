c820886168a6b31f178a8686931ffae38949269b
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-10-31 10:57:46 -0500

    Update Question, Options
    Option button correct or wrong color on click

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,12 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 
-const initialState = { questions: [], status: "loading", index: 0 };
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+};
 
 function reducer(state, action) {
   switch (action.type) {
@@ -17,13 +22,15 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      return { ...state, answer: action.payload };
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -45,7 +52,13 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,9 +1,22 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <ul className="options">
-      {question.options.map((option) => (
+      {question.options.map((option, index) => (
         <li key={option}>
-          <button className="btn btn-option">{option}</button>
+          <button
+            className={`btn btn-option ${index === answer ? "answer" : ""} ${
+              hasAnswered
+                ? index === question.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
+            } `}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+            disabled={hasAnswered}
+          >
+            {option}
+          </button>
         </li>
       ))}
     </ul>
